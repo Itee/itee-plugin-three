e45e104f41b46d33757b0990bf6c7282fb6843a9
Tristan VALCKE <valcketristan@gmail.com>
2019-08-14 17:46:20 +0200

    fix(cameracontrols): fix invalid cameracontrolmode enum instance

--- sources/frontend/controllers/CameraControls.js
+++ sources/frontend/controllers/CameraControls.js
@@ -285,7 +285,7 @@ class CameraControls extends EventDispatcher {
 
         if ( isNull( value ) ) { throw new Error( 'Mode cannot be null ! Expect a value from CameraControlMode enum.' ) }
         if ( isUndefined( value ) ) { throw new Error( 'Mode cannot be undefined ! Expect a value from CameraControlMode enum.' ) }
-        if ( !( value instanceof CameraControlMode ) ) { throw new Error( `Mode cannot be an instance of ${value.constructor.name}. Expect a value from TCameraControlMode enum.` ) }
+        //        if ( !( value instanceof CameraControlMode ) ) { throw new Error( `Mode cannot be an instance of ${value.constructor.name}. Expect a value from TCameraControlMode enum.` ) }
 
         this._mode = value
 
