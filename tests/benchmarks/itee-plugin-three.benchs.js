e2fcbf102a6007ad8529c86aecd911440ce57069
Tristan VALCKE <valcketristan@gmail.com>
2025-09-08 21:28:09 +0200

    build(npm): npm run pre-release

--- tests/benchmarks/itee-plugin-three.benchs.js
+++ tests/benchmarks/itee-plugin-three.benchs.js
@@ -1,22 +1,8 @@
-/**
- * @author [Tristan Valcke]{@link https://github.com/Itee}
- * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
- *
- */
 
-/* global suite, benchmark */
+const suites = [
 
-const IteeUtilsSuite = suite( 'Itee#Utils', () => {
+]
 
-    benchmark(
-        'isArray()',
-        function () {
-
-            return true
-
-        },
-        {} )
-
-} )
-
-export { IteeUtilsSuite }
+for ( const suite of suites ) {
+	suite.run()
+}
