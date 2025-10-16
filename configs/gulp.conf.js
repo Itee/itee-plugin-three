31cd689731ea87a85182f6585de2a6af24191fc9
Tristan VALCKE <valcketristan@gmail.com>
2025-10-16 22:12:34 +0200

    feat(gulpfile): use latest itee standard about gulpfile with external config file

---
+++ configs/gulp.conf.js
@@ -0,0 +1,45 @@
+import { readFileSync } from 'fs'
+
+const packageInfos = JSON.parse( readFileSync(
+    new URL( '../package.json', import.meta.url )
+) )
+
+const config = {
+    'clean':              [
+        './builds',
+        './tests/units',
+        './tests/benchmarks',
+        './docs'
+    ],
+    'lint':               [
+        'configs/**/*.js',
+        'sources/**/*.js',
+        'tests/**/*.js',
+        '!tests/**/builds/*.js',
+        '!tests/others/*.js'
+    ],
+    'doc':                [
+        'README.md',
+        'gulpfile.mjs',
+        './configs/*.js',
+        './sources/**/*.js',
+        './tests/**/*.js'
+    ],
+    'check-bundling':     [
+        `${ packageInfos.name }.js`
+    ],
+    'compute-unit-tests': [
+        `${ packageInfos.name }.js`
+    ],
+    'compute-benchmarks': [
+        `${ packageInfos.name }.js`
+    ],
+}
+
+function getGulpConfigForTask( taskName ) {
+
+    return config[ taskName ]
+
+}
+
+export { getGulpConfigForTask }
