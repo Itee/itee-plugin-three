4dcf705f469777cf16e77430693c7757cc344b08
Tristan VALCKE <valcketristan@gmail.com>
2025-10-20 02:06:25 +0200

    :recycle: (builds) update build task and related configuration to latest standards

--- configs/gulp.conf.mjs
+++ configs/gulp.conf.mjs
@@ -1,8 +1,16 @@
-import { readFileSync } from 'fs'
+import { readFileSync }  from 'fs'
+import {
+    dirname,
+    join
+}                        from 'path'
+import { fileURLToPath } from 'url'
+
 
 const packageInfos = JSON.parse( readFileSync(
     new URL( '../package.json', import.meta.url )
 ) )
+const __filename   = fileURLToPath( import.meta.url )
+const __dirname    = dirname( __filename )
 
 const config = {
     'clean':              [
@@ -35,6 +43,14 @@ const config = {
     'compute-benchmarks': [
         `${ packageInfos.name }.js`
     ],
+    'builds':             {
+        input:     join( __dirname, '../sources', `${ packageInfos.name }.js` ),
+        output:    join( __dirname, '../builds' ),
+        formats:   [ 'esm', 'cjs', 'iife' ],
+        envs:      [ 'dev', 'prod' ],
+        sourcemap: true,
+        treeshake: true
+    }
 }
 
 function getGulpConfigForTask( taskName ) {
